1aa63ea5137c01ac6e35df4b53ab9b6d1d5ae488
Siddhartha <sidduinevitable@gmail.com>
2020-05-16 21:25:04 +0530

    solution for test program

---
+++ TEST/main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+#define endl "\n"
+#define IOS                \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0);
+
+using namespace std;
+
+int main() {
+  int n;
+  while (true) {
+    cin >> n;
+    if (n == 42) break;
+    cout << n << endl;
+  }
+}
