003a5ac8995f309933046e6da081bc2105b2c951
Siddhartha <sidduinevitable@gmail.com>
2020-05-18 08:09:21 +0530

    add missing equality condition and convert to long long type

--- FCTRL/main.cpp
+++ FCTRL/main.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 
 #define endl "\n"
+#define ll long long
 #define IOS                \
   ios::sync_with_stdio(0); \
   cin.tie(0);              \
@@ -14,12 +15,12 @@ int main() {
   cin >> T;
 
   while (T--) {
-    int n;
+    ll n;
     cin >> n;
 
-    int k = 5;
-    int sum = 0;
-    while (n > k) {
+    ll k = 5;
+    ll sum = 0;
+    while (n >= k) {
       sum += n / k;
       k *= 5;
     }
